63955280cc09cec7fbd7fa9253d0e1da3ff839f5
Renz-Programmer <figuerrarenz12@gmail.com>
2026-03-22 12:16:11 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
 
+// ✅ ADD THIS LINE
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
